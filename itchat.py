2e6db49cc7bd2480281a141d1158060c7bb8c3f4
ron159 <zlronron159@gmail.com>
2018-08-10 14:09:40 +0800

    Update itchat.py

--- itchat.py
+++ itchat.py
@@ -1,10 +1,9 @@
 import itchat
 import led
 
-#itchat框架，关注TEXT消息
+
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-    #得到任何text消息就打开流水灯，最后原消息返回
     led.openLed()
     print(msg.text)
     return msg.text
