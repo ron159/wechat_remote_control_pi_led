942d937e8785780aa5124d2d337ac190049b8797
ron159 <zlronron159@gmail.com>
2018-08-10 20:39:38 +0800

    0.3

--- wechat2pi.py
+++ wechat2pi.py
@@ -4,9 +4,14 @@ import itchat
 
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
+    if msg['Text'] == "闪一闪":
     #led.openLed()
-    print(msg.text)
-    return msg.text
+    #print(msg.text)
+        reply_content = "正在闪一闪"
+        itchat.send(reply_content,toUserName=msg['FromUserName'])
+    else:
+        pass
+
 
 itchat.auto_login(enableCmdQR=2)
 itchat.run() 
